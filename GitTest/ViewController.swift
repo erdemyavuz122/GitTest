e322927f2f9c7a45e6fc3f1b95dd6f67cea64d0b
Erdem <eerdemyavuz@gmail.com>
2024-07-16 11:19:54 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Erdem Yavuz on 16.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var a = 0
+
+}
+
